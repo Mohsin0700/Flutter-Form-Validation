ab793a860d1cb21c28b01b3892893eec9d08edff
Mohsin0700 <muhammad786mohsin024@gmail.com>
2025-08-24 07:43:48 +0500

    Files are updated

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -29,70 +29,77 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(title: Text("Registration Form")),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Form(
-                key: _formKey,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      'Register',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: MyTextformField(
-                            controller: firstNameController,
-                            hint: 'First Name',
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Expanded(
-                          child: MyTextformField(
-                            controller: lastNameController,
-                            hint: 'Last Name',
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    MyTextformField(controller: emailController, hint: 'Email'),
-                    SizedBox(height: 10),
-                    MyTextformField(
-                      controller: passwordController,
-                      hint: 'Password',
-                    ),
-                    SizedBox(height: 10),
-
-                    MyTextformField(
-                      controller: confirmPasswordController,
-                      hint: 'Confirm Password',
-                    ),
-
-                    ElevatedButton(
-                      onPressed: () {
-                        print("Login button pressed");
-                      },
-                      style: ButtonStyle(
-                        shape: WidgetStatePropertyAll<OutlinedBorder>(
-                          RoundedRectangleBorder(),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: _formKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Column(
+                    // mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Register',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      child: Text("Register"),
-                    ),
-                  ],
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: MyTextformField(
+                              controller: firstNameController,
+                              hint: 'First Name',
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: MyTextformField(
+                              controller: lastNameController,
+                              hint: 'Last Name',
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      MyTextformField(
+                        controller: emailController,
+                        hint: 'Email',
+                      ),
+                      SizedBox(height: 10),
+                      MyTextformField(
+                        controller: passwordController,
+                        hint: 'Password',
+                        isPassword: true,
+                      ),
+                      SizedBox(height: 10),
+
+                      MyTextformField(
+                        controller: confirmPasswordController,
+                        hint: 'Confirm Password',
+                      ),
+
+                      ElevatedButton(
+                        onPressed: () {
+                          print("Login button pressed");
+                        },
+                        style: ButtonStyle(
+                          shape: WidgetStatePropertyAll<OutlinedBorder>(
+                            RoundedRectangleBorder(),
+                          ),
+                        ),
+                        child: Text("Register"),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/widgets/my_textform_field.dart
+++ lib/widgets/my_textform_field.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class MyTextformField extends StatefulWidget {
   final TextEditingController controller;
   final String hint;
+  final bool isPassword;
   const MyTextformField({
     super.key,
     required this.controller,
     required this.hint,
+    this.isPassword = false,
   });
 
   @override
@@ -31,12 +33,17 @@ class _MyTextformFieldState extends State<MyTextformField> {
         errorText = "Password must be at least 8 characters";
       });
       print(errorText);
+    } else {
+      setState(() {
+        errorText = null;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: widget.isPassword,
       textInputAction: TextInputAction.next,
       controller: widget.controller,
       decoration: InputDecoration(
@@ -44,6 +51,16 @@ class _MyTextformFieldState extends State<MyTextformField> {
         border: OutlineInputBorder(),
         errorText: errorText,
       ),
+      onChanged: (value) {
+        if (widget.isPassword) {
+          passwordValidator(value);
+        }
+      },
+      onFieldSubmitted: (value) {
+        if (widget.isPassword) {
+          passwordValidator(value);
+        }
+      },
     );
   }
 }
